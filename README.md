299c3be210dcf2f6ad6037ad9491bb121a2d9240
Robert Jackson <robert.w.jackson@me.com>
2014-04-23 21:53:12 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Broccoli Timepiece
+
+It's what you use to watch things, you know?
+
+## Usage
+
+Uses the standard `broccoli` watcher to build a tree (from the `Brocfile.js` in your projects root), and output to a directory.
+
+Pass the name of the directory to output to as the first commandline parameter.
+
+```bash
+broccoli-timepiece dist/
+```
+
+## License
+
+This project is distributed under the MIT license.
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var fs       = require('fs');
+var path     = require('path');
+var chalk    = require('chalk');
+var rimraf   = require('rimraf');
+var helpers  = require('broccoli-kitchen-sink-helpers')
+var Watcher  = require('broccoli/lib/watcher');
+var broccoli = require('broccoli');
+
+function createWatcher(destDir) {
+  var tree    = broccoli.loadBrocfile();
+  var builder = new broccoli.Builder(tree);
+  var watcher = new Watcher(builder);
+
+  watcher.on('change', function(srcDir) {
+    rimraf.sync(destDir);
+    helpers.copyRecursivelySync(srcDir, destDir);
+
+    var message = "Build successful";
+
+    if (builder.buildTime) {
+      message += ' - ' + builder.buildTime + 'ms.\n';
+    }
+    console.log(chalk.green(message));
+  });
+
+  watcher.on('error', function(err) {
+    console.log(chalk.red('\n\nBuild failed.\n'));
+  });
+
+  return watcher;
+}
+
+createWatcher(process.argv[2]);
