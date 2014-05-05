263ba9b44b7ec42ae74ee65c6e9a07589949b219
Robert Jackson <robert.w.jackson@me.com>
2014-05-04 21:22:32 -0400

    Implement locking with `lockfile`.
    This depends on the changes in broccoli PR #141.

--- index.js
+++ index.js
@@ -2,29 +2,66 @@
 
 var fs       = require('fs');
 var path     = require('path');
+var RSVP     = require('rsvp');
 var chalk    = require('chalk');
-var rimraf   = require('rimraf');
 var helpers  = require('broccoli-kitchen-sink-helpers');
 var Watcher  = require('broccoli/lib/watcher');
+var lockfile = require('lockfile');
 var broccoli = require('broccoli');
 
-function createWatcher(destDir, interval) {
-  var tree    = broccoli.loadBrocfile();
-  var builder = new broccoli.Builder(tree);
-  var watcher = new Watcher(builder, {interval: interval || 100});
+var RSVP      = require('rsvp');
+var denodeify = RSVP.denodeify;
+var rimraf    = denodeify(require('rimraf'));
+var lock      = denodeify(lockfile.lock);
+var unlock    = denodeify(lockfile.unlock);
 
-  watcher.on('change', function(results) {
-    rimraf.sync(destDir);
-    helpers.copyRecursivelySync(results.directory, destDir);
+var destDir      = process.argv[2];
+var interval     = process.argv[3];
+var lockFilename = '.broccoli-timepiece';
 
-    console.log(chalk.green("Build successful - " + Math.floor(results.totalTime / 1e6) + 'ms'));
-  });
+function willBuild() {
+  return lock(lockFilename);
+};
 
-  watcher.on('error', function(err) {
-    console.log(chalk.red('\n\nBuild failed.\n'));
-  });
-
-  return watcher;
+function didBuild(results) {
+  rimraf(destDir)
+    .then(function() {
+      return helpers.copyRecursivelySync(results.directory, destDir);
+    })
+    .finally(function() {
+      return unlock(lockFilename);
+    })
+    .finally(function() {
+      console.log(chalk.green("Build successful - " + Math.floor(results.totalTime / 1e6) + 'ms'));
+    });
 }
 
-createWatcher(process.argv[2], process.argv[3]);
+function didError(error) {
+  console.log(chalk.red('\n\nBuild failed.\n'));
+  console.error(err.stack);
+
+  return unlock(lockFilename);
+}
+
+var tree    = broccoli.loadBrocfile();
+var builder = new broccoli.Builder(tree);
+var watcher = new Watcher(builder, {
+  interval: interval || 100,
+  willBuild: willBuild,
+  didBuild: didBuild,
+  didError: didError,
+});
+
+process.addListener('exit', function () {
+  builder.cleanup();
+  unlock(lockFilename);
+});
+
+// We register these so the 'exit' handler removing temp dirs is called
+process.on('SIGINT', function () {
+  process.exit(1);
+});
+
+process.on('SIGTERM', function () {
+  process.exit(1);
+});
