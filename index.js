5bbb6cd73029fcfee4937694a06fd9f1aeacc03b
Jonathan Hawk <jonathan@hawk.ninja>
2016-12-12 13:39:46 -0500

    Now works with Broccoli 1.1

--- index.js
+++ index.js
@@ -4,36 +4,42 @@ var fs       = require('fs');
 var path     = require('path');
 var chalk    = require('chalk');
 var rimraf   = require('rimraf');
-var helpers  = require('broccoli-kitchen-sink-helpers');
-var Watcher  = require('broccoli/lib/watcher');
+var copyDereferenceSync = require('copy-dereference').sync;
 var broccoli = require('broccoli');
+var printSlowNodes = require('broccoli-slow-trees');
+var program = require('commander');
 
-function createWatcher(destDir, interval) {
+function createWatcher(destDir, options) {
   var tree    = broccoli.loadBrocfile();
   var builder = new broccoli.Builder(tree);
-  var watcher = new Watcher(builder, {interval: interval || 100});
-
-  var atExit = function() { 
-    builder.cleanup()
-      .then(function() {
-        rimraf.sync("./broccoli-timepiece-failure.json");
-        process.exit(1);
-      });
+  var saneOptions = {
+      watchman: options.watchman === true,
+      poll: options.poll !== undefined,
+      interval: options.poll
   };
-  
+  var watcher = new broccoli.Watcher(builder, {saneOptions: saneOptions});
+
+  var atExit = function() {
+    watcher.quit();
+    rimraf.sync("./broccoli-timepiece-failure.json");
+  };
+
   process.on('SIGINT', atExit);
   process.on('SIGTERM', atExit);
 
-  watcher.on('change', function(results) {
+  watcher.on('buildSuccess', function(results) {
     rimraf.sync(destDir);
     rimraf.sync("./broccoli-timepiece-failure.json");
 
-    helpers.copyRecursivelySync(results.directory, destDir);
+    copyDereferenceSync(builder.outputPath, destDir);
 
-    console.log(chalk.green("Build successful - " + Math.floor(results.totalTime / 1e6) + 'ms'));
+    if (options.verbose) {
+        printSlowNodes(builder.outputNodeWrapper);
+    }
+    console.log(chalk.green("Build successful - " + builder.outputNodeWrapper.buildState.totalTime + 'ms'));
   });
 
-  watcher.on('error', function(error) {
+  watcher.on('buildFailure', function(error) {
     // Output to the console
     console.log(chalk.red('\n\nBuild failed.'));
 
@@ -55,7 +61,7 @@ function createWatcher(destDir, interval) {
 
     // Write a JSON dump file
     rimraf.sync("./broccoli-timepiece-failure.json");
-    fs.appendFile("./broccoli-timepiece-failure.json", JSON.stringify({
+    fs.appendFileSync("./broccoli-timepiece-failure.json", JSON.stringify({
       message: error.message,
       file: error.file,
       treeDir: error.treeDir,
@@ -65,7 +71,35 @@ function createWatcher(destDir, interval) {
     }, null, 2));
   });
 
+  watcher.start()
+    .catch(function(err) {
+      console.log(err && err.stack || err)
+    })
+    .finally(function() {
+      builder.cleanup()
+    })
+    .catch(function(err) {
+      console.log('Cleanup error:')
+      console.log(err && err.stack || err)
+    })
+    .finally(function() {
+      process.exit(1)
+    });
+
   return watcher;
 }
 
-createWatcher(process.argv[2], process.argv[3]);
+program
+  .version(JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8')).version)
+  .usage('[options] <destination>')
+  .option('-v, --verbose', 'Enable verbose output')
+  .option('-w, --watchman', 'Use the Watchman watcher')
+  .option('-p, --poll <interval>', 'Use the polling watcher at the specified interval in milliseconds', parseInt)
+  .parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+  process.exit(1);
+}
+
+createWatcher(program.args[0], program);
